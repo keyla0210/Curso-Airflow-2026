5c8b527a7f3cd83c0bb24ab1f88fae378a978423
jdravila <jdravila@bancolombia.com.co>
2026-01-31 17:47:04 -0500

    Agregar ajustes al laboratorio uno

--- A_Laboratorios/A_Lab_Intro_to_Airflow/dags/elt_medallon.py
+++ A_Laboratorios/A_Lab_Intro_to_Airflow/dags/elt_medallon.py
@@ -10,12 +10,6 @@ if str(PROJECT_ROOT) not in sys.path:
     sys.path.append(str(PROJECT_ROOT))
 
 from elt.ingest_raw import ingest_to_raw
-from elt.bronze import copy_raw_to_bronze
-from elt.silver import transform_bronze_to_silver
-from elt.fact_episodes import build_fact_episodes
-from elt.dim_time import build_dim_time
-from elt.dim_shows import build_dim_shows 
-from elt.dim_networks import build_dim_networks
 
 BOGOTA_TZ = pendulum.timezone("America/Bogota")
 
@@ -25,59 +19,12 @@ DATA_LAKE_ROOT = Path("/opt/airflow/data_lake")
 #Capa raw
 RAW_ROOT = DATA_LAKE_ROOT / "raw" / "tvmaze"
 
-#Capa bronze
-BRONZE_PATH = DATA_LAKE_ROOT / "bronze" / "tvmaze"
-
-#Capa Silver 
-SILVER_PATH = DATA_LAKE_ROOT / "silver" / "tvmaze"
-
-#Fact episdodes
-FACT_EPISODES_PATH = DATA_LAKE_ROOT / "gold" / "facts" / "episodes.parquet"
-
-#Dim Time
-DIM_TIME_PATH = DATA_LAKE_ROOT / "gold" / "dimensions" / "time.parquet"
-#Dim Show
-DIM_SHOW_PATH = DATA_LAKE_ROOT / "gold" / "dimensions" / "show.parquet"
-#Dim Network
-DIM_NETWORK_PATH = DATA_LAKE_ROOT / "gold" / "dimensions" / "network.parquet"
-
 INGEST_PARAMS = {
     "start_date": pendulum.date(2020, 1, 1),
     "end_date": pendulum.date(2020, 1, 31),
     "output_dir": str(RAW_ROOT),
     "timeout": 30,
 }
-
-BRONZE_PARAMS = { 
-    "raw_root": str(RAW_ROOT),
-    "bronze_path": str(BRONZE_PATH / "tvmaze.parquet"),
-}
-
-SILVER_PARAMS ={
-    "bronze_path": str(BRONZE_PATH / "tvmaze.parquet"),
-    "silver_path": str(SILVER_PATH / "tvmaze_silver.parquet"),
-}
-
-EPISODES_PARAMS ={
-    "silver_path": str(SILVER_PATH / "tvmaze_silver.parquet"),
-    "output_path": str(FACT_EPISODES_PATH),
-}
-
-DIM_TIME_PARAMS ={
-    "silver_path": str(SILVER_PATH / "tvmaze_silver.parquet"),
-    "output_path": str(DIM_TIME_PATH),
-}
-
-DIM_SHOW_PARAMS ={
-    "silver_path": str(SILVER_PATH / "tvmaze_silver.parquet"),
-    "output_path": str(DIM_SHOW_PATH),
-}
-
-DIM_NETWORK_PARAMS ={
-    "silver_path": str(SILVER_PATH / "tvmaze_silver.parquet"),
-    "output_path": str(DIM_NETWORK_PATH),
-}
-
 
 with DAG(
 
@@ -93,35 +40,4 @@ with DAG(
         op_kwargs=INGEST_PARAMS,
     )
 
-    bronze_task = PythonOperator(
-        task_id="copy_to_bronze",
-        python_callable=copy_raw_to_bronze,
-        op_kwargs=BRONZE_PARAMS,
-    )
-    silver_task = PythonOperator(
-        task_id="to_silver",
-        python_callable=transform_bronze_to_silver,
-        op_kwargs=SILVER_PARAMS,
-    )
-    time_task = PythonOperator(
-        task_id="dim_time",
-        python_callable=build_dim_time,
-        op_kwargs=DIM_TIME_PARAMS,
-    )
-    show_task = PythonOperator(
-        task_id="dim_show",
-        python_callable=build_dim_shows,
-        op_kwargs=DIM_SHOW_PARAMS,
-    )
-    network_task = PythonOperator(
-        task_id="dim_network",
-        python_callable=build_dim_networks,
-        op_kwargs=DIM_NETWORK_PARAMS,
-    )
-    episodes_task = PythonOperator(
-        task_id="fact_episodes",
-        python_callable=build_fact_episodes,
-        op_kwargs=EPISODES_PARAMS,
-    )
-
-    ingest_task >> bronze_task >> silver_task >> [time_task, show_task, network_task] >> episodes_task
+    ingest_task
