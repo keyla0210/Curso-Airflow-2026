e59c79c420dc3dbfeae27ce0c221988338324fb0
jdravila <jdravila@bancolombia.com.co>
2026-02-11 21:51:02 -0500

    hacer cambio en el host

--- A_Laboratorios/B_Lab_Celery_Executor/dags/celery.py
+++ A_Laboratorios/B_Lab_Celery_Executor/dags/celery.py
@@ -4,24 +4,24 @@ from time import sleep
 @dag
 def celery_dag():
     
-    @task()
+    @task
     def a():
         sleep(5)
         
     @task(
-       
+       queue="high_cpu"
     )
     def b():
         sleep(5)
 
     @task(
-       
+       queue="high_cpu"
     )
     def c():
         sleep(5)
         
     @task(
-        
+       queue="high_cpu"
     )
     def d():
         sleep(5)
